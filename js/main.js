2eb92118e287065d341c46b90c5a34362541ca46
Eunbin Lee <dmsqlsdl54@naver.com>
2022-10-20 15:08:23 +0900

    animation

--- js/main.js
+++ js/main.js
@@ -36,6 +36,7 @@ window.addEventListener('scroll', _.throttle(function () {
 }, 300));
 // _.throttle(함수, 시간)
 
+
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
   // gsap.to(요소, 지속시간, 옵션);
@@ -44,6 +45,7 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1
   });
 });
+
 
 // new Swiper(선택자, 옵션)
 new Swiper('.notice-line .swiper-container', {
@@ -84,3 +86,28 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionEl.classList.remove('hide');
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector,   // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {  // 옵션
+      y: size,
+      repeat: -1,   // 무한루프
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
